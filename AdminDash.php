be40f2c057f97d02f4f160c3f18af37b22446f46
Spring <jsprng@healthcare.uiowa.edu>
2024-02-14 15:20:42 -0600

    Diagnose exec view.

--- AdminDash.php
+++ AdminDash.php
@@ -476,7 +476,7 @@ class AdminDash extends AbstractExternalModule
             // error_log(json_encode($reportProps));
             // error_log(json_encode($reportAccess));
             // echo json_encode($reportAccess);
-            $projectView = $reportAccess[$reportProps[0]["report_id"]]["project_view"];
+            $projectView = $reportAccess[$_GET["id"]]["project_view"];
 
  
             // error_log(json_encode($projectView));
